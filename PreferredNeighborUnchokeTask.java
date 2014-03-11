80790593f1cb6ee76c5687899a1c0bc7e427049a
Kenkron <Kenkron@gmail.com>
2014-03-11 11:19:43 -0400

    PreferredNeighborUnchokeTask has some problems.  They have not been fixed, but they have been commented for further review.

--- PreferredNeighborUnchokeTask.java
+++ PreferredNeighborUnchokeTask.java
@@ -5,6 +5,10 @@ public class PreferredNeighborUnchokeTask extends TimerTask {
     public PreferredNeighborUnchokeTask() {
     }
 
+    //@ TODO:
+    //most likely, the following method will need to create a sorted list
+    //of RPI based on getDataRcvd, and take the tow highest of that list.
+    
 	/** This method is called by peerProcess according to a given 
 	 * interval. This method will find the k preferred neighbors. */
     public void run() {
@@ -14,12 +18,16 @@ public class PreferredNeighborUnchokeTask extends TimerTask {
         for (RemotePeerInfo rpi : peerProcess.peerList) {
 			//Get the corresponding peerHandler for this RPI
 			PeerHandler peer = peerProcess.rpiToPeerHandler.get(rpi); 
+			// @ TODO: does not account for null piers
 			if (peer.otherPeerIsInterested) {
 				//Check if this peer has better (or equal?) rates than 
 				//current preferred neighbors
 				for (int i = 0; i < peerProcess.NumberOfPreferredNeighbors; i++) {
 					if (peer.getDataRcvd() >= dataRates[i]) {
 						dataRates[i] = peer.getDataRcvd();
+						// @ TODO: the following assignment is incorrect
+						// the highest peer will be put in all the slots
+						// and will tend to override the fastest first
 						preferredNeighbors[i] = rpi;
 					}
 				}
