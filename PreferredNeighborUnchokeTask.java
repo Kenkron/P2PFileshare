43e7ddd33068dead32addd514245d4f92e49a614
Sachit Mishra <smishra2@ufl.edu>
2014-03-13 12:47:05 -0400

    Removed old implementation

--- PreferredNeighborUnchokeTask.java
+++ PreferredNeighborUnchokeTask.java
@@ -19,25 +19,33 @@ public class PreferredNeighborUnchokeTask extends TimerTask {
         //after this method is executed. 
         ArrayList<RemotePeerInfo> preferredList = new ArrayList<RemotePeerInfo>();
         
-        //for each of k preferred neighbors, find highest in list so far.
-        for (int i = 0; i < peerProcess.NumberOfPreferredNeighbors; i++) {
-            RemotePeerInfo best = null;
-            int bestRate = 0;
-            //go through each peer to see how it compares to current best
-            for (RemotePeerInfo rpi : peerProcess.peerList) {
-                PeerHandler peer = peerProcess.rpiToPeerHandler.get(rpi);
-                if (peer != null && peer.otherPeerIsInterested) {
-                    //if it is better...
-                    if (peer.getDataRcvd() >= bestRate) {
-                        best = rpi;
-                        bestRate = peer.getDataRcvd();
+        //first check if we have less peers than # of allowable preferred neighbors
+        if (peerList.size() <= peerProcess.NumberOfPreferredNeighbors) {
+            //if so, just make everyone a preferred neighbor
+            Collections.copy(preferredList, peerList);
+        }
+        //otherwise, check for k best
+        else {
+            //for each of k preferred neighbors, find highest in list so far.
+            for (int i = 0; i < peerProcess.NumberOfPreferredNeighbors; i++) {
+                RemotePeerInfo best = null;
+                int bestRate = 0;
+                //go through each peer to see how it compares to current best
+                for (RemotePeerInfo rpi : peerProcess.peerList) {
+                    PeerHandler peer = peerProcess.rpiToPeerHandler.get(rpi);
+                    if (peer != null && peer.otherPeerIsInterested) {
+                        //if it is better...
+                        if (peer.getDataRcvd() >= bestRate) {
+                            best = rpi;
+                            bestRate = peer.getDataRcvd();
+                        }
                     }
                 }
+                //Now place the best neighbor in the preferred list and remove 
+                //it from list of peers which are being considered
+                preferredList.add(best);
+                peerList.remove(best);
             }
-            //Now place the best neighbor in the preferred list and remove 
-            //it from list of peers which are being considered
-            preferredList.add(best);
-            peerList.remove(best);
         }
         
         //Now clear data counters for all neighbors
@@ -47,33 +55,5 @@ public class PreferredNeighborUnchokeTask extends TimerTask {
         //TODO: Send choke/unchoke messages (make sure to log these? or does the peerHandler do that?)
         
      }
-    
-	/** Version 1: This method is called by peerProcess according to a given 
-	 * interval. This method will find the k preferred neighbors. */
-//    public void run() {
-//		// Find the k fastest, interested neighbors
-//		int[] dataRates = new int[peerProcess.NumberOfPreferredNeighbors];
-//		RemotePeerInfo[] preferredNeighbors = new RemotePeerInfo[peerProcess.NumberOfPreferredNeighbors];
-//        for (RemotePeerInfo rpi : peerProcess.peerList) {
-//			//Get the corresponding peerHandler for this RPI
-//			PeerHandler peer = peerProcess.rpiToPeerHandler.get(rpi); 
-//			// @ TODO: does not account for null piers
-//			if (peer.otherPeerIsInterested) {
-//				//Check if this peer has better (or equal?) rates than 
-//				//current preferred neighbors
-//				for (int i = 0; i < peerProcess.NumberOfPreferredNeighbors; i++) {
-//					if (peer.getDataRcvd() >= dataRates[i]) {
-//						dataRates[i] = peer.getDataRcvd();
-//						// @ TODO: the following assignment is incorrect
-//						// the highest peer will be put in all the slots
-//						// and will tend to override the fastest first
-//						preferredNeighbors[i] = rpi;
-//					}
-//				}
-//			}
-//			peer.clearDataCounter();
-//		}
-//		
-//		//TODO: Send choke/unchoke messages (make sure to log these? or does the peerHandler do that?)
-//    }
+
 }
